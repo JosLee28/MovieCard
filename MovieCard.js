c56fa17b41d76ea8f3bb57d91c6fe4053163e119
Josue lee <josuelee28@gmail.com>
2021-10-01 19:38:47 -0600

    Menu Responsive

--- MovieCard.js
+++ MovieCard.js
@@ -4,12 +4,14 @@ const start = () => {
 
 function btnHamburger() {
   const $btn = document.getElementById("btn-hamburger");
+  const $menu = document.getElementById("menu");
   document.addEventListener("click", (e) => {
     if (
       e.target.matches("#btn-container") ||
       e.target.matches("#btn-container *")
     ) {
       $btn.classList.toggle("is-active");
+      $menu.classList.toggle("main-menu--responsive");
     }
   });
 }
